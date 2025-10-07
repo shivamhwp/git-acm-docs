57c034b07dce4863804b7d0bcb261104cdb02325
Shivam Sharma <91240327+shivamhwp@users.noreply.github.com>
2025-10-07 17:43:57 +0530

    minor changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,41 +6,46 @@ import starlightThemeRapide from "starlight-theme-rapide";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      plugins: [starlightThemeRapide()],
-      title: "git-acm docs",
-      social: [
-        {
-          icon: "github",
-          label: "github",
-          href: "https://github.com/shivamhwp/git-acm",
-        },
-        {
-          icon: "x.com",
-          label: "x.com",
-          href: "https://twitter.com/shivamhwp",
-        },
-      ],
-      sidebar: [
-        {
-          label: "guides",
-          items: [
-            { label: "installation", slug: "guides/installation" },
-            { label: "configuration", slug: "guides/configuration" },
-          ],
-        },
-        {
-          label: "reference",
-          autogenerate: { directory: "reference" },
-        },
-        {
-          label: "faq",
-          autogenerate: { directory: "faq" },
-        },
-      ],
-      customCss: ["./src/tailwind.css"],
-    }),
-    tailwind({ applyBaseStyles: false }),
-  ],
+	integrations: [
+		starlight({
+			plugins: [starlightThemeRapide()],
+			title: "docs",
+			logo: {
+				src: "./src/assets/logo.png",
+				alt: "git-acm logo",
+			},
+			favicon: "./src/assets/favicon.ico",
+			social: [
+				{
+					icon: "github",
+					label: "github",
+					href: "https://github.com/shivamhwp/git-acm",
+				},
+				{
+					icon: "x.com",
+					label: "x.com",
+					href: "https://twitter.com/shivamhwp",
+				},
+			],
+			sidebar: [
+				{
+					label: "guides",
+					items: [
+						{ label: "installation", slug: "guides/installation" },
+						{ label: "configuration", slug: "guides/configuration" },
+					],
+				},
+				{
+					label: "reference",
+					autogenerate: { directory: "reference" },
+				},
+				{
+					label: "faq",
+					autogenerate: { directory: "faq" },
+				},
+			],
+			customCss: ["./src/tailwind.css"],
+		}),
+		tailwind({ applyBaseStyles: false }),
+	],
 });
